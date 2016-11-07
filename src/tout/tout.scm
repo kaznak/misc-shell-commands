a13b4f5d2e4f59ee5e32952428be6f03acdef926
kaznak <kaznak@gmail.com>
2016-11-07 13:41:54 +0900

    Add tout Prototype

---
+++ src/tout/tout.scm
@@ -0,0 +1,33 @@
+
+(use extras)
+(use posix)
+
+(define (%separate-stime-line l)
+  (call-with-input-string
+   l
+   (lambda(s)
+     (let*((t (read s))
+	   (l (read-line s)) )
+       (if (eq? l #!eof) (set! l ""))
+       (cons t l) ))))
+
+(define (%static-read-sleep-out s iport oport)
+  (let loop ((l (read-line iport)))
+    (cond ((string? l)
+	   (sleep s)(write-line l oport)
+	   (loop (read-line iport)) )
+	  ((eq? #!eof l) 0)
+	  (else 1) )))
+
+(define (%read-sleep-out iport oport)
+  (let loop ((l (read-line iport)))
+    (cond ((string? l)
+	   (let((x(%separate-stime-line l))) ; this should be try-catch.
+	     (sleep (car x))
+	     (write-line (cdr x) oport) )
+	   (loop (read-line iport)) )
+	  ((eq? #!eof l) 0)
+	  (else 1) )))
+
+;; (%static-read-sleep-out 1 (current-input-port) (current-output-port))
+(%read-sleep-out (current-input-port) (current-output-port))
